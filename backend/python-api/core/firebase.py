d40688267779a5296d1ce5a40f847dd8e48115be
Krushna Ganesh Pokharkar <160628406+Krushna2142@users.noreply.github.com>
2026-02-12 16:10:44 +0530

    allowedorigins

--- backend/python-api/core/firebase.py
+++
@@ -1,9 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials, firestore, auth
-import os
-
-if not firebase_admin._apps:
-    cred = credentials.Certificate("firebase/serviceAccount.json")
-    firebase_admin.initialize_app(cred)
-
-db = firestore.client()
