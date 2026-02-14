65a5fbb6f6179aa0dcd31c69558b33af2cce5184
Krushna Ganesh Pokharkar <160628406+Krushna2142@users.noreply.github.com>
2026-02-14 05:45:30 +0530

    API Changes

--- frontend/lib/axios.ts
+++ frontend/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { auth } from '@/lib/firebase/Client';
 
 const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL ?? 'http://localhost:8000';
+  process.env.NEXT_PUBLIC_API_URL ;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
