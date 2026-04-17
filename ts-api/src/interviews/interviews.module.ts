63e397a7449c2075dd53e587b17c5d6acb1a5e34
Krushna Ganesh Pokharkar <160628406+Krushna2142@users.noreply.github.com>
2026-04-17 14:46:43 +0530

    new code

--- ts-api/src/interviews/interviews.module.ts
+++ ts-api/src/interviews/interviews.module.ts
@@ -1,22 +1,36 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PrismaService } from '../../prisma/prisma.service';
-import { InterviewsService } from './interviews.service';
-import { InterviewGateway } from './interview.gateway';
+
 import { InterviewsController } from './interviews.controller';
+import { InterviewGateway } from './interview.gateway';
+import { InterviewsService } from './interviews.service';
+
 import { RecruiterInterviewsController } from './recruiter-interviews.controller';
+import { RecruiterInterviewsService } from './recruiter-interviews.service';
+
 import { CandidateInterviewsController } from './candidate-interviews.controller';
 import { InterviewRoomController } from './interview-room.controller';
 
+import { DatabaseModule } from '../database/datbase.module';
+
 @Module({
-  imports: [JwtModule.register({})],
+  imports: [
+    JwtModule.register({}),
+    DatabaseModule, // required because RecruiterInterviewsService injects DatabaseService
+  ],
   controllers: [
     InterviewsController,
     RecruiterInterviewsController,
     CandidateInterviewsController,
     InterviewRoomController,
   ],
-  providers: [PrismaService, InterviewsService, InterviewGateway],
-  exports: [InterviewsService],
+  providers: [
+    PrismaService,
+    InterviewsService,
+    RecruiterInterviewsService, // <-- missing provider (main crash reason)
+    InterviewGateway,
+  ],
+  exports: [InterviewsService, RecruiterInterviewsService],
 })
 export class InterviewsModule {}
